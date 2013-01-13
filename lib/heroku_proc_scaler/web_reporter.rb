bd20c60f01e5b3b00633c34058cb50e9835b0022
Chris Hart <hartct@gmail.com>
2013-01-12 23:26:09 -0500

    fixes

--- lib/heroku_proc_scaler/web_reporter.rb
+++ lib/heroku_proc_scaler/web_reporter.rb
@@ -6,6 +6,8 @@ module HerokuProcScaler
 
     def initialize
       @connection = ActiveRecord::Base.connection
+      delete_existing_stats
+      @connection.insert_sql "INSERT INTO procscaler_web_stats (dyno_guid, last_queue_depth, created_at, updated_at) VALUES ('#{HerokuProcScaler.configuration.dyno_guid}', 0, TIMESTAMP '#{Time.now.to_s(:db)}', TIMESTAMP '#{Time.now.to_s(:db)}')", nil, 'dyno_guid'
     end
 
     def current_queue_depth depth
@@ -28,7 +30,14 @@ module HerokuProcScaler
 
     def update_stats_record depth
       puts "updating queue depth to #{depth}"
-      @connection.insert_sql "INSERT INTO procscaler_web_stats (dyno_guid, last_queue_depth, created_at, updated_at) VALUES ('#{HerokuProcScaler.configuration.dyno_guid}', #{depth.to_i}, TIMESTAMP '#{Time.now.to_s(:db)}', TIMESTAMP '#{Time.now.to_s(:db)}')", nil, 'dyno_guid'
+      begin
+        @connection.update_sql "UPDATE procscaler_web_stats SET last_queue_depth = #{depth.to_i}, updated_at = TIMESTAMP '#{Time.now.to_s(:db)}' WHERE dyno_guid = '#{HerokuProcScaler.configuration.dyno_guid}'"
+        @last_report_time = Time.now.to_i
+      rescue Exception => e
+        puts "exception "
+        puts e.message
+        puts e.backtrace.inspect
+      end
     end
 
   end
