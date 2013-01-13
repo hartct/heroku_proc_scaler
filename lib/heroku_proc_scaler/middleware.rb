f45a6914e281d13e8be032e51a954e0d0a5f0cc7
Chris Hart <hartct@gmail.com>
2013-01-12 22:45:44 -0500

    Removed eventmachine

--- lib/heroku_proc_scaler/middleware.rb
+++ lib/heroku_proc_scaler/middleware.rb
@@ -16,15 +16,16 @@ module HerokuProcScaler
     end
 
     def call(env)
-      EM.run do
+      #EM.run do
         begin
-          EM.defer { scale(env) }
+          #EM.defer { scale(env) }
+          scale(env)
         rescue Exception => e
           puts e.message
           puts e.backtrace.inspect
         end
         app.call(env)
-      end
+      #end
     end
 
     def scale(env)
